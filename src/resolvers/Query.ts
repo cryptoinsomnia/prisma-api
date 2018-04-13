c5a84d7ee06ee9c596004031f7f4e8a6760a58b6
Francesco Agosti <francesco.agosti93@gmail.com>
2018-04-12 22:09:33 -0700

    Implement hacker news ranking in feed query if no ordering is specified

--- src/resolvers/Query.ts
+++ src/resolvers/Query.ts
@@ -1,8 +1,48 @@
+import * as moment from 'moment';
 import { Context, getUserId } from '../utils/utils';
 
+// The information we need about a Post to determine its' score.
+const rankingInfoQuery = `{
+  id
+  createdAt
+  votes {
+    id
+  }
+}`;
+
 export const Query = {
-  feed(parent, { first, orderBy }, ctx: Context, info) {
-    return ctx.db.query.posts({ first, orderBy }, info);
+  async feed(parent, { first, orderBy }, ctx: Context, info) {
+    if (orderBy) {
+      return ctx.db.query.posts({ first, orderBy }, info);
+    }
+    // Max age of a post is 1 week
+    const oneWeekAgo = moment().subtract(1, 'week').toISOString();
+    const posts = await ctx.db.query.posts({
+      orderBy: 'createdAt_DESC',
+      where: {
+        createdAt_gte: oneWeekAgo,
+      },
+    }, rankingInfoQuery);
+    // As seen https://medium.com/hacking-and-gonzo/how-hacker-news-ranking-algorithm-works-1d9b0cf2c08d
+    const idToScore = posts.reduce((acc, post) => {
+      const points = post.votes.length;
+      const createdAt = moment(post.createdAt);
+      const hoursSinceSubmission = moment.duration(moment().diff(createdAt)).asHours();
+      const gravity = 1.8;
+      acc[post.id] = points / ((hoursSinceSubmission + 2) ** gravity);
+      return acc;
+    }, {});
+    const scoreComparator = (post1, post2) => idToScore[post2.id] - idToScore[post1.id];
+    // Sort posts by score.
+    const sortedPosts = posts.sort(scoreComparator);
+    // Find the ids for the top "first" (provided by query) posts.
+    const topIds = sortedPosts.slice(0, first).map((post) => post.id);
+    // We have to return the information the query requested.
+    const topPostsWithRequestedFields = await ctx.db.query.posts({ where: {
+      id_in: topIds,
+    }}, info);
+    // We have to sort again because we are not guaranteed ordering.
+    return topPostsWithRequestedFields.sort(scoreComparator);
   },
 
   user(parent, { username }, ctx: Context, info) {
